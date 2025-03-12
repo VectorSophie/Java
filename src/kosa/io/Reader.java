7e04342788242083895889cc2eb3b338bccf1bfa
VectorSophie <jay7math@gmail.com>
2025-03-12 14:29:07 +0900

    i can read...better!

--- src/kosa/io/Reader.java
+++ src/kosa/io/Reader.java
@@ -5,12 +5,13 @@ import java.io.FileReader;
 public class Reader {	
 	public static void main(String[] args) {
 		FileReader reader = null;
+		char arr[] = new char[256];
 		try {
 			reader = new FileReader("poem.txt");
 			while (true) {
-				int data = reader.read();
+				int data = reader.read(arr);
 				if (data == -1) break;
-				System.out.println((char)data);
+				System.out.println(arr);
 			}
 		} catch(Exception e) {
 		
