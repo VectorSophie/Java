9093d6477ffb552757330adde4bcd05a551b3b8f
VectorSophie <jay7math@gmail.com>
2025-03-12 09:06:35 +0900

    f

--- src/kosa/data/Matematica.java
+++ src/kosa/data/Matematica.java
@@ -4,14 +4,29 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Matematica {
+	public static boolean MathMatch(String str) {
+		Stack <String> Math = new Stack<String>();
+		
+			if (str.contains("(") ) {
+				Math.push("(");
+			} else if (str.contains(")") ) {
+				if(Math.isEmpty()) {
+					return false;
+				}
+				Math.pop();
+				}
+			return Math.isEmpty();
+	}
+	
+
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Stack <Integer> Math = new Stack<Integer>();
-		String input = "(2+3)*5 = 25";
-		if (input.contains("(") ) {
-
-		} else if (input.contains(")") ) {
-
+		String input = "(2+3)))*5 = 25";
+		if(MathMatch(input)) {
+			System.out.println("예~");
+		} else {
+			System.out.println("아니오~");
 		}
+	
 	}
 }
