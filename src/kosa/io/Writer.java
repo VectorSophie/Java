bc7f34c6d97e44d7f5f73ac0a391c03b02df2a61
VectorSophie <jay7math@gmail.com>
2025-03-12 14:42:23 +0900

    anglophiles

---
+++ src/kosa/io/Writer.java
@@ -0,0 +1,35 @@
+package kosa.io;
+
+import java.util.Scanner;
+import java.io.FileWriter;
+
+public class Writer {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		FileWriter writer = null;
+		String str = "";
+		String poem = "";
+		System.out.println("입력: ");
+		while(!(str = sc.nextLine()).equals("q")) {
+			poem += str;
+			poem += "\n";
+			
+			try {
+				writer = new FileWriter("poem2.txt");
+				writer.write(poem);
+			} catch(Exception e) {
+			
+			} finally {
+				try {
+					writer.close();
+				} catch (Exception e2) {
+					
+				}
+			}
+		}
+		System.out.println("종료");
+		System.out.println(poem);
+	}
+
+}
