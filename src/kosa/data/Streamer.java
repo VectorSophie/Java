e732f0df15266165bf1808079abc9d69a420f453
VectorSophie <jay7math@gmail.com>
2025-03-12 11:51:42 +0900

    garfield

--- src/kosa/data/Streamer.java
+++ src/kosa/data/Streamer.java
@@ -9,7 +9,26 @@ public class Streamer {
 	public static void main(String[] args) {
 		List<Integer> numbers = IntStream.rangeClosed(1,10).filter(n -> n%2==0).map(n -> n * n).boxed().collect(Collectors.toList());
 		
-		List<Integer> squared = numbers.stream().filter(n -> n%2==0).map(n -> n * n).collect(Collectors.toList());
+		List<Integer> squared = numbers.stream().
+				filter(n -> n%2==0).map(n -> n * n).
+				collect(Collectors.toList());
 		System.out.println(numbers);
+		
+		List<Student> list = Arrays.
+				asList(new Student("È«±æµ¿", 90), 
+						new Student("È«ÁöÀº", 100),
+						new Student("±è±æµ¿", 70));
+		
+		list.stream().
+		mapToInt(s -> s.getScore()).
+		forEach(score -> System.out.println(score));
+		
+		list.stream().map(Student::getName).
+		forEach(name -> System.out.println(name));
+		
+		list.stream().
+		filter(n-> n.getName().charAt(0) == 'È«').
+		forEach(Student -> System.out.println(Student.getName()));
+		
 	}
 }
